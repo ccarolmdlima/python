a99b5d19b6bae61ff9cbdd28070f6cbd2f8b9ccc
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-07 09:30:38 -0300

    Create funcao01.py

---
+++ funcao01.py
@@ -0,0 +1,12 @@
+#nota >= 9 - A
+#nota = 8 - B
+#nota = 7 - C
+
+n1 = 9.5; n2 = 8.5; n3 = 7.5
+a1 = "pedro"; a2 = "felipe"; a3 = "joaquim"
+if n1 >= 9.0:
+    print("conceito A para o aluno: ", a1)
+if n2 >= 8.0 and n2 < 9.0:
+    print("conceito B para o aluno: ", a2)
+if n3 >= 7.0 and n3 < 8.0:
+    print("conceito C para o aluno: ", n3)
