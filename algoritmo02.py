16f02bff4a952ba1ec56c93cc987a68423f500a4
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-07 08:33:41 -0300

    Update algoritmo02.py

--- algoritmo02.py
+++ algoritmo02.py
@@ -4,3 +4,13 @@ l = [5,4,3,2,8,7,4,9] #int
 #1) imprima os 3 últimos elementos
 #2) ordene a em ordem crescente e decrescente
 #3) insira um elemento no início da l
+
+print(l[5])
+print(l[6])
+print(l[7])
+l.sort()
+print(l)
+l.sort(reverse=True)
+print(l)
+l.insert(0, 11)
+print(l)
