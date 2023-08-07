b6aa4a20af9f54b14ec24a3c553aed9f868ca2d9
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-07 09:40:08 -0300

    Update funcao01.py

--- funcao01.py
+++ funcao01.py
@@ -10,3 +10,15 @@ if n2 >= 8.0 and n2 < 9.0:
     print("conceito B para o aluno: ", a2)
 if n3 >= 7.0 and n3 < 8.0:
     print("conceito C para o aluno: ", n3)
+
+#usando for
+n = [9.5, 8.5, 7.5]
+a = ['pedro', 'felipe', 'joaquim']
+
+for i in range(len(n)):
+    if n[i] >= 9.0:
+        print('conceito A para o aluno: ', a[i])
+    if n[i] >= 8.0 and n[i] < 9.0:
+        print('conceito B para o aluno: ', a[i])
+    if n[i] >= 7.0 and n[i] < 8.0:
+        print("conceito C para o aluno: ", n[i])
