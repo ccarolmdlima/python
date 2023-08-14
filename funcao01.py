17abb6cbe582b8f7c4e9d9548889b5aa5942cf26
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-14 09:01:22 -0300

    Update funcao01.py

--- funcao01.py
+++ funcao01.py
@@ -2,28 +2,27 @@ nota = []
 nome = []
 
 def lenotanome():
-    x = int(input("digite a quantidade de alunos: "))
+    x = int(input("Digite a quantidade de alunos: "))
     for i in range(x):
-        print("aluno {}" .format(i))
-        n = float(input("digite a nota do aluno: "))
-        m = input("digite o nome do aluno: ")
+        print("Aluno {}".format(i))
+        n = float(input("Digite a nota do aluno: "))
+        m = input("Digite o nome do aluno: ")
         nota.append(n)
         nome.append(m)
 
 def avalianota():
     for i in range(len(nome)):
-        if nota[i] >= 9.0 and nota[i] <= 10:
-            print("o aluno {} tem conceito A e nota {}" .format(nome[i]
-            ,nota[i]))
-        if nota[i] < 9.0 and nota[i] >= 8.0:
-            print("o aluno {} tem conceito B e nota {}" .format(nome[i]
-            ,nota[i]))
-        if nota[i] >= 7.0 and nota[i] < 8.0:
-            print("o aluno {} tem conceito C e nota {}" .format(nome[i]
-            ,nota[i]))
+        if 9.0 <= nota[i] <= 10:
+            print("O aluno {} tem conceito A e nota {}".format(nome[i], nota[i]))
+        elif 8.0 <= nota[i] < 9.0:
+            print("O aluno {} tem conceito B e nota {}".format(nome[i], nota[i]))
+        elif 7.0 <= nota[i] < 8.0:
+            print("O aluno {} tem conceito C e nota {}".format(nome[i], nota[i]))
 
-print("o programa começa aqui")
+print("O programa começa aqui")
 lenotanome()
-print("meio do programa")
+print("Meio do programa")
 avalianota()
-print("fim do programa")
+print("Fim do programa")
+print("Nomes:", nome)
+print("Notas:", nota)
