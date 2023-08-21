d1a402a7b7a9955aa091ceda98f5c5053e6155c7
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-21 08:30:11 -0300

    Update elevarapotencia01.py

--- elevarapotencia01.py
+++ elevarapotencia01.py
@@ -2,3 +2,14 @@ x = int(input("digite a base: "))
 y = int(input("digite o expoente: "))
 
 print(pow(x, y))
+
+------------------------------------------------------
+
+import math
+
+x = int(input("digite a base: "))
+y = int(input("digite o expoente: "))
+
+print(pow(x, y))
+
+print(math.sqrt(9))
