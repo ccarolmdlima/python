e017ef412985972cb291b1833d1d705460cdc082
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-21 09:02:31 -0300

    Update funcao03.py

--- funcao03.py
+++ funcao03.py
@@ -41,3 +41,30 @@ entrada()
 
 print(pow(x, y))
 print(math.sqrt(9))
+
+
+#correção do código
+import math
+
+def pot(x, y):
+    i = y 
+    z = 1
+    while i > 0:
+        i = i - 1
+        z = z * x
+    return z
+
+def entrada():
+    try:
+        x = int(input("Digite a base: "))
+        y = int(input("Digite o expoente: "))
+        return x, y  # Retorna os valores para serem usados fora da função
+    except ValueError:
+        print("Erro! Digite apenas números")
+        return None, None
+
+x, y = entrada()  # Obtém os valores da função entrada()
+
+if x is not None and y is not None:
+    print(pot(x, y))
+    print(math.sqrt(9))
