73d7e125b233b6ecaff74ff603801181167524f7
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-21 08:42:59 -0300

    Update tratamentodeerros02.py

--- tratamentodeerros02.py
+++ tratamentodeerros02.py
@@ -10,3 +10,17 @@ except ValueError:
     print("entrada inválida. certifique-se de inserir números inteiros para a base e o expoente.")
 except Exception as e:
     print(f"ocorreu um erro inesperado: {e}")
+
+-------------------------------------------------------------------------------------------------------------------------
+
+import math
+
+while True:
+    try:
+        x = int(input("digite a base: "))
+        y = int(input("digite o expoente: "))
+        break
+    except:
+        print("erro! digite apenas números")
+
+print(pow(x, y))
