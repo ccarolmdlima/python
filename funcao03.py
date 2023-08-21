d331fe4bfab567061eb736514cd11c1d88e7d4a6
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-08-21 09:00:27 -0300

    Update funcao03.py

--- funcao03.py
+++ funcao03.py
@@ -15,3 +15,29 @@ z = pot(x, y)
 print(z)
 
 print(math.sqrt(9))
+
+---------------------------------------------------------------------------------------------------
+
+#função que retorna o valor da potência
+
+import math
+
+def pot(x, y):
+    i = y 
+    z = 1
+    while i > 0:
+        i = i - 1
+        z = z * x
+    return z
+
+def entrada():
+   try:
+        x = int(input("digite a base: "))
+        y = int(input("digite o expoente: "))
+   except:
+        print("erro! digite apenas números")
+
+entrada()
+
+print(pow(x, y))
+print(math.sqrt(9))
