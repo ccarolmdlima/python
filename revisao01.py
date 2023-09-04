d17c1d179e3e2722233f9cdbe3b41342cb75d21f
carol <126472626+ccarolmdlima@users.noreply.github.com>
2023-09-04 10:35:17 -0300

    Update revisao01.py

--- revisao01.py
+++ revisao01.py
@@ -6,3 +6,22 @@
 #imprimir os alunos com suas notas
 #alterar a nota de um aluno do dicionário
 #teclar <sair> ou <SAIR> para abandonar o programa
+
+def menu():
+    while True:
+        print("criar uma lista")
+        op = input("digite a opção: ")
+        if op == 'sair'
+        break
+        
+def crialista():
+    print("função cria lista")
+    pass
+    #return(lista
+
+def inverterLista(lista):
+    print("A uniao B", a | b)
+    print("A interseção B", a & b)
+    print("diferença simétrica ", a^b)
+    print("B pertence a ", b in a)
+    print("2 pertence a ", 2 in a)
